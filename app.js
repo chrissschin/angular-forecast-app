12630b5bf97ebf40db34d38216b72fc1303fc95a
chrissschin <solischris92@gmail.com>
2016-01-28 16:54:23 -0800

    added temperature

--- app.js
+++ app.js
@@ -41,9 +41,26 @@ weatherApp.controller('homeCtrl',['$scope', 'cityService', function($scope, city
 }]);
 
 
-weatherApp.controller('forecastCtrl', ['$scope', 'cityService', function($scope, cityService) {
+weatherApp.controller('forecastCtrl', ['$scope', '$resource' ,'cityService', function($scope,$resource, cityService) {
     
     $scope.city = cityService.city;
+    
+    $scope.weatherApi = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", 
+    { callback: "JSON_CALLBACK" }, 
+    { get: { method: "JSONP" }});
+    
+    $scope.weatherResult = $scope.weatherApi.get({
+        q: $scope.city,
+        cnt: 7,
+        appid: "f3988637806bfbb1e77b057704da9cc3"     
+    });
+    
+    
+    $scope.convertToFarenheit = function(degK) {
+        
+       return Math.round((1.8 * (degK - 273)) + 32);
+       
+    }
     
 }]);
 
